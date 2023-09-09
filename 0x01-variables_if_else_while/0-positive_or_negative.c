f783d98288bdeccdb8454a87e417367876caea54
Anifu-ngugi <anifungugi1@gmail.com>
2023-09-09 03:23:01 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -19,9 +19,9 @@ int main(void)
 	scanf("%d", &n);
 
 	if (n > 0)
-		printf("%d  is positive", n);														     else if (n == 0)
-		printf("%d  is zero", n);
+		printf("%d is positive\n", n);														     else if (n == 0)
+		printf("%d is zero\n", n);
 	else 
-		printf("%d  is negative", n);
+		printf("%d is negative\n", n);
 	return (0);
 }
