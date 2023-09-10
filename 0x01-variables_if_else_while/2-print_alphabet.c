64a390553510c0128791408b44264b253671f4dc
Anifu-ngugi <anifungugi1@gmail.com>
2023-09-10 12:28:45 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main-entry point
+ *
+ * Description: A program using putchar to display alphabets in lowercase
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+    char ch;
+
+    for (ch = 'a'; ch <= 'z'; ch++)
+    {
+        putchar(ch);
+    }
+
+    return (0);
+}
