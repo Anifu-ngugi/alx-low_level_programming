269c23fbac76bb77c89840cd224184fc6cc32e9f
Anifu-ngugi <anifungugi1@gmail.com>
2023-09-11 13:41:17 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main-entry point
+ *
+ * Description:using continue to skip certain letters
+ *
+ * Return:Always 0 (success))
+ */
+int main(void)
+{
+int i;
+
+for (i = 0 ; i < 10 ; i++)
+{
+putchar('0' + i);
+}
+putchar('\n');
+return (0);
+}
