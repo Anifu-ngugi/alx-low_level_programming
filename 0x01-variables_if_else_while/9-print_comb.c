cbefc01e39945ca3561d9422bc86000dbeb6399f
Anifu-ngugi <anifungugi1@gmail.com>
2023-09-11 15:12:43 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main-entry point
+ *
+ * Description:using continue to skip certain letters
+ *
+ * Return:Always 0 (success))
+ */
+int main(void)
+{
+int i;
+
+for (i = 0 ; i < 10 ; i++)
+{
+putchar('0' + i);
+
+if (i < 9)
+{
+putchar(',');
+putchar(' ');
+}
+else
+{
+putchar('$');
+}
+}
+putchar('\n');
+return (0);
+}
