07487794491dec3e0d04d94250e2681f4cff89ed
Anifu-ngugi <anifungugi1@gmail.com>
2023-09-11 00:26:19 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,28 +1,28 @@
 #include <stdio.h>
 
 /**
- * main-entry point
- *
- * Description: A program using putchar to display alphabets in lowercase
- *
- * Return: Always 0 (Success)
- */
+* main-entry point
+*
+* Description: A program using putchar to display alphabets in lowercase
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-    char ch;
+char ch;
 
-    for (ch = 'a'; ch <= 'z'; ch++)
-    {
-        putchar(ch);
-    }
-   
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+putchar(ch);
+}
 
-    for (ch = 'A'; ch <= 'Z'; ch++)
-    {
-        putchar(ch);
-    }
-   putchar('\n');  /* Print a newline*/ 
 
-    return 0; /* Return 0 to indicate success*/
+for (ch = 'A'; ch <= 'Z'; ch++)
+{
+putchar(ch);
+}
+putchar('\n');  /* Print a newline*/
+
+return (0); /* Return 0 to indicate success*/
 }
 
