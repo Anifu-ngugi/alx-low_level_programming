25c9ca6eb89d1c0239d64b9a5949b831bb206a0b
Anifu-ngugi <anifungugi1@gmail.com>
2023-09-12 15:13:08 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main-entry point
+ *
+ * Description:using continue to skip certain letters
+ *
+ * Return:Always 0 (success))
+ */
+int main(void)
+{
+	int i;
+
+	for(i = 'a' ; i <= 'z' ; i++)
+	{
+		_putchar('i');
+		return (0);
+	}
+	_putchar('\n');
+
+}
