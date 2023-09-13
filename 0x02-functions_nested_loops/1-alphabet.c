ea2972967bf2ba0d41b37ca6cdf92ef3dec905d5
Anifu-ngugi <anifungugi1@gmail.com>
2023-09-13 03:54:20 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - entry point
+ *
+ * Description:print alphabets in lowercase
+ *
+ * Return:always 0 (success)
+ */
+int main(void)
+{
+	char k;
+
+	for (k = 'a' ; k <= 'z' ; k++)
+	{
+		_putchar('k');
+		return (0);
+	}
+_putchar('\n');
+}
