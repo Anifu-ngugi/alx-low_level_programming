88742a3dcc1c8e77b44e3f86e56582c05ded9941
Anifu-ngugi <anifungugi1@gmail.com>
2023-09-14 14:54:49 -0700

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -5,14 +5,16 @@
  * Return: return 0
  **/
 void print_line(int n)
-{
+{	
+	int i;
+
 	if (n <= 0)
 	{
 		_putchar('\n');
 	}
 	else
 	{
-		for (int i = 0; i < n; i++)
+		for (i = 0; i < n; i++)
 		{
 		_putchar(95);
 		}
