44e8fcd8797e636a55b34f36926b1d74eb1e3f55
Anifu-ngugi <anifungugi1@gmail.com>
2023-09-14 14:49:55 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_line- prints straight line
+ * @n: number of times the n line with be printed
+ * Return: return 0
+ **/
+void print_line(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (int i = 0; i < n; i++)
+		{
+		_putchar(95);
+		}
+		_putchar('\n');
+	}
+}
