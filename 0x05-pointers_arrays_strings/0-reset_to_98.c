16407ea7bf65a3846710ff95bbdb55233bb63d7c
Anifu-ngugi <anifungugi1@gmail.com>
2023-09-18 05:40:42 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98- a function that points to a int and updated it to 98
+ * @n: the parameter to be computed
+ * Return: return void 0
+ **/
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
