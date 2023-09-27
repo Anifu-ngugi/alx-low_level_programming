26753e7255ca349c07d5af5c4b75d482bc97706b
Anifu-ngugi <anifungugi1@gmail.com>
2023-09-27 14:26:52 -0700

    1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _print_rev_recursion- function that prints a strings in reversal
+ * @s: computed string
+ * Return: void 0
+ **/
+void _print_rev_recursion(char *s)
+{
+        if (*s == '\0')
+        {
+                return;
+        }
+       _print_rev_recursion(s + 1);/**recusively call the function with the nxt char**/
+       _putchar(*s);
+}
