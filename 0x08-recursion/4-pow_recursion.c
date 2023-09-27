9413a330bda45ebcf3e232afab59a17989e6911a
Anifu-ngugi <anifungugi1@gmail.com>
2023-09-27 15:27:25 -0700

    4-pow_recursion.c

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *  _pow_recursion- func that prints x power y
+ *  @x: parameter to be computed
+ *  @y: the parameter to be computed
+ *  Return: rerurns int
+ **/
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return(-1);
+	}
+		return (x * _pow_recursion(x,y - 1));
+}
