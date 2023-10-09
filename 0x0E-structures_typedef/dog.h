ec00bc6fc3261b87254f90e4baa4a8d420e9a4bb
Anifu-ngugi <anifungugi1@gmail.com>
2023-10-09 14:12:05 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+#include <stdio.h>
+/**
+ * struct dog - shows a dog description
+ * @name: first member
+ * @age: second member
+ * @owner: third member
+ * Description: it fully shows the dog characterists
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
